498eae2981a0f065375e5618d11c77e1941604c2
Omar <milton.yv98@gmail.com>
2025-06-21 12:16:28 -0500

    variables

---
+++ ejercicio1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+main(){
+  printf("hola \cmundo\n"); // el programa evita \ y solo escribe c y eesto no ocasiona nada en el programa
+  
+  return 0;
+}
--- hola_mundo.c
+++ hola_mundo.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> //Incluye informacion acerca de la biblioteca estandar
 
-main() {
-  printf("Hola, mundo con c\n");
+main() {  // define una funcion llamada main que no recibe valores de argumentos
+  printf("Hola, mundo con c\n"); // main llama a la funcion de biblioteca printf. para escribir esta secuencia
+  printf("Hola, mundo con c\n"); // main llama a la funcion de biblioteca printf. para escribir esta secuencia
 
-  return 0;
+  printf("Hola,");
+  printf("mundo\n");
+
+  return 0; // desde actualizaciones recientes un programa siempre debe retornar algo
 }
